116db6ad51d2515cb2580dfbcc4350b456356789
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2025-07-13 20:01:52 +0300

    add new rewrite rule

--- Sekmen.StaticSiteGenerator/GlobalUsings.cs
+++ Sekmen.StaticSiteGenerator/GlobalUsings.cs
@@ -6,6 +6,7 @@ global using System.IO;
 global using System.Net;
 global using System.Net.Http;
 global using System.Net.Http.Headers;
+global using System.Text;
 global using System.Text.RegularExpressions;
 global using System.Threading.Tasks;
 global using System.Xml.Linq;
--- Sekmen.StaticSiteGenerator/Program.cs
+++ Sekmen.StaticSiteGenerator/Program.cs
@@ -9,10 +9,14 @@ using var client = new HttpClient();
 
 _ = await Functions.DownloadSingleFile(client, url, outputFolder, "robots.txt", newDomain);
 _ = await Functions.DownloadSingleFile(client, url, outputFolder, "manifest.json", newDomain);
+_ = await Functions.DownloadSingleFile(client, url, outputFolder, "404.html", newDomain);
 var sitemapXml = await Functions.DownloadSingleFile(client, url, outputFolder, "sitemap.xml", newDomain);
 
-//add 404
-sitemapXml = sitemapXml.Replace("</urlset>", "<url><loc>" + newDomain + "404</loc></url></urlset>");
+//add 404 and articles pages
+var urls = new StringBuilder();
+for (var i = 2; i <= 4; i++)
+    urls.Append("<url><loc>" + url + "articles/page/" + i + "</loc></url>");
+sitemapXml = sitemapXml.Replace("</urlset>", urls + "</urlset>");
 
 //convert sitemap tp xml
 var sitemap = XDocument.Parse(sitemapXml);
@@ -20,9 +24,7 @@ var ns = sitemap.Root!.GetDefaultNamespace();
 var xElements = sitemap.Descendants(ns + "loc").ToArray();
 
 foreach (var urlElement in xElements)
-{
     await Functions.ProcessUrls(client, urlElement, outputFolder, url, newDomain);
-}
 
 Console.WriteLine("Done.");
 
